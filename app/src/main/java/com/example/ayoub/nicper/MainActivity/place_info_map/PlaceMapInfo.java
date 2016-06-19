161820a8c994730390a4904ac4ad1dce4f1413be
Admin <ayoubdroid96@gmail.com>
2016-06-19 15:14:03 -0400

    info place screen

--- app/src/main/java/com/example/ayoub/nicper/MainActivity/place_info_map/PlaceMapInfo.java
+++ app/src/main/java/com/example/ayoub/nicper/MainActivity/place_info_map/PlaceMapInfo.java
@@ -1,22 +1,82 @@
 package com.example.ayoub.nicper.MainActivity.place_info_map;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ayoub.nicper.Object.Map.PlaceInfo;
 import com.example.ayoub.nicper.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class PlaceMapInfo extends AppCompatActivity {
+public class PlaceMapInfo extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
 
+    private TextView owner;
+    private TextView price;
+    private TextView priceInfo;
+    private TextView availability;
+    private TextView availabilityInfo;
+    private MapFragment mapFragment;
+    private double lat, lng;
+    private GoogleMap mMap;
+    private LatLng currentLocation;
+    private PlaceInfo placeInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_map_info);
+
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    private void initialise(){
         Intent i = getIntent();
-        PlaceInfo placeInfo = (PlaceInfo) i.getSerializableExtra("placeInfo");
+        placeInfo = (PlaceInfo) i.getSerializableExtra("placeInfo");
+        currentLocation = new LatLng(placeInfo.getLat(), placeInfo.getLng());
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                            .position(currentLocation));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
+        this.mMap = googleMap;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
