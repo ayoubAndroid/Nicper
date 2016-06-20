e011098cc657f32674a7a8bca6dc50eb11f90f50
ayoubAndroid <ayoubdroid96@gmail.com>
2016-06-20 01:13:03 -0400

    chat

---
+++ app/src/main/java/com/example/ayoub/nicper/MainActivity/chat_message/ChatActivity.java
@@ -0,0 +1,15 @@
+package com.example.ayoub.nicper.MainActivity.chat_message;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ayoub.nicper.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
